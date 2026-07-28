c6b0a06f12d79df420f7f0752d453bdf56f5abf5
GroupDev-Web <mogan30@saintstephenschool.org>
2026-07-27 21:56:44 -0400

    Refactor sorting function in quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -3,16 +3,18 @@ import * as ExternalPlugin from "./.quartz/plugins"
 
 ExternalPlugin.Explorer({
   sortFn: (a, b) => {
-    // Put pages above folders
     if (a.isFolder !== b.isFolder) {
       return a.isFolder ? 1 : -1
     }
 
-    // Sort pages alphabetically and folders alphabetically
-    return (a.displayName ?? "").localeCompare(b.displayName ?? "", undefined, {
-      numeric: true,
-      sensitivity: "base",
-    })
+    return (a.displayName ?? "").localeCompare(
+      b.displayName ?? "",
+      undefined,
+      {
+        numeric: true,
+        sensitivity: "base",
+      },
+    )
   },
 })
 
