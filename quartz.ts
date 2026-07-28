64eae6e1e3737c771392f00c2d064e320c6f8b28
GroupDev-Web <mogan30@saintstephenschool.org>
2026-07-27 21:21:57 -0400

    Add sorting function for Explorer plugin

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,22 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+ExternalPlugin.Explorer({
+  sortFn: (a, b) => {
+    if (a.isFolder === b.isFolder) {
+      return a.displayName.localeCompare(b.displayName, undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+    }
+
+    // Pages first, folders second
+    return a.isFolder ? 1 : -1
+  },
+})
 
 const config = await loadQuartzConfig()
+
 export default config
+
 export const layout = await loadQuartzLayout()
